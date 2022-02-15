1022fddffba6617df529c5134315ae34abe5c56e
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-02-15 14:42:26 +0900

    template内の要素でわかるところを追加

--- src/views/BaseballTeamDetail.vue
+++ src/views/BaseballTeamDetail.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    
+    <div class="list">球団名</div>
+    {{ team.teamName }}
+    <div class="list">本拠地</div>
+    {{ team.headquarters }}
+    <div class="list">発足日</div>
+    <div class="list">歴史</div>
+    {{ team.history }}
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { Team } from "@/types/Team";
+import { Hotel } from "@/types/Hotel";
+import { Clothe } from "@/types/Clothe";
 @Component
-export default class XXXComponent extends Vue {
-
-}
+export default class BaseballTeamDetail extends Vue {}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
